aab2ad092f773c6f4e3a8d59f50913ba3da9242a
Qirui Liu <liuqirui42x@gmail.com>
2024-06-03 12:26:46 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -51,16 +51,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+     {
+        title: 'About',
+        pageId: 'b148d3983bbf42f18761ee0d6faa0d45'
+     },
+     {
+       title: 'Contact',
+       pageId: 'a77d8bf37e86491dac2c3fffc4432f6e'
+     }
+   ]
 })
