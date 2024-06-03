bfc7d49e0766b3c0e0b989b738ea1bba73dcabf3
Qirui Liu <liuqirui42x@gmail.com>
2024-06-03 12:16:46 +0800

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href='https://github.com/Cyanometer67/nextjs-notion-starter-kit'
+      href='https://github.com/Cyanometer67'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
